679b7d245e54447b58acecdfa6dec036554989ca
dlcparadox <nylanderjoshuah@gmail.com>
2025-04-22 20:57:20 -0300

    Adicionados tipos de Variáveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,17 @@
-public class App {
+public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // Tipos primitivos
+        String nome = "Joshuah"; // Defini um texto
+        boolean verdadeira = true; //Defini se é verdadeiro ou falso
+        char letra = 'J'; // Defini uma letra
+        byte numeroByte = 127; //Defini um número inteiro de 8 bits
+        short numeroShort = 32767; //Defini um número inteiro de 16 bits
+        int idade = 26; //Defini um número inteiro de 32 bits
+        double salariominimo = 2500; //Defini um número inteiro de 64 bits
+
+        //Geralmente não se usa float, byte e short, pois o int é mais rápido e ocupa menos espaço na memória.
+
+        final String DEFINICAO = "Não muda";
+        // final é um tipo de variável que não pode ser alterado depois de definido.
     }
 }
